9a72cad44ae9fc054e96b72c70469a52cc97e48c
Mile Stojakovic <kordunas2002@gmail.com>
2025-11-22 13:29:46 -0500

    Add constructors, make classes JSON serializable

--- components.py
+++ components.py
@@ -1,15 +1,41 @@
-from enum import Enum
+import json
+import datetime as dt
 
 class Category:
     name: str
     color: str
 
+    def __init__(self, name: str, color: str):
+        self.name = name
+        self.color = color
+
 class Entry:
     name: str
     amount: float
     category: Category
+    date: dt.date
+
+    def __init__(self, name: str, amount: float, category: Category, date: dt.date):
+        self.name = name
+        self.amount = amount
+        self.category = category
+        self.date = date
 
 class Account:
     name: str
     balance: float
     entries: list[Entry]
+    categories: list[Category]
+
+    def __init__(self, name: str):
+        self.name = name
+        self.balance = 0
+        self.entries = []
+        self.categories = [Category("Uncategorized", "gray")]
+
+def pyftJSONSerializer(object):
+    if isinstance(object, Category):
+        return object.name
+    elif isinstance(object, dt.date):
+        return object.isoformat()
+    return object.__dict__
